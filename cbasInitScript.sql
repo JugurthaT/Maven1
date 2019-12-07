54173eb4be0f9e67a469a701b5289965239b1733
JugurthaT <JugurthaT@gmail.com>
2019-12-07 15:42:44 +0300

    Remove some directories

--- cbasInitScript.sql
+++
@@ -1,3 +0,0 @@
-create bucket developerBucket with { "bucket": "default", "nodes": "127.0.0.1" };
-create shadow dataset developer on developerBucket;
-connect bucket developerBucket with { "password": "" }
--- gittalent-frontend/src/environments/environment.test.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-production: false,
-  gittalentBackendURL:"http://gittalentbackend:8080"
-};
